32f4dc3d3bda5a2b76850b8416b28f8b0b03d7c2
knda-dev <cjlim000523@gmail.com>
2024-11-28 23:20:55 +0900

    url

--- kiosk/src/index.js
+++ kiosk/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Menu from './menu'
+import { Router } from 'express';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Menu />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Router>
+      <Router path='/' element={<Menu/>}/>
+    </Router>
+  </BrowserRouter>
+  // <React.StrictMode>
+  //   <Menu />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
